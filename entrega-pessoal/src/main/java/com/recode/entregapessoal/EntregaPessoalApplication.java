132bfee2d19521ff43e28b40febabad6f6623c33
Thiago da Gama Fernandes <thigama.f@hotmail.com>
2022-11-03 10:42:04 -0300

    criando a API

---
+++ entrega-pessoal/src/main/java/com/recode/entregapessoal/EntregaPessoalApplication.java
@@ -0,0 +1,13 @@
+package com.recode.entregapessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaPessoalApplication.class, args);
+	}
+
+}
