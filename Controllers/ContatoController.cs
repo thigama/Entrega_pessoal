8c68feff592b10e624f4a85ba3546b0cd4743ae7
Thiago da Gama Fernandes <106341987+thigama@users.noreply.github.com>
2022-11-13 16:08:18 -0300

    api c#

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Website.Models;
+
+namespace Website.Controllers;
+
+public class ContatoController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    
+}
