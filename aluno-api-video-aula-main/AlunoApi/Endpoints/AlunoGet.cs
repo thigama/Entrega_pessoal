c8d794ada4cd90eee0954b3a9a1b25fd0d86a797
Thiago da Gama Fernandes <thigama.f@hotmail.com>
2022-11-13 12:45:40 -0300

    .

---
+++ aluno-api-video-aula-main/AlunoApi/Endpoints/AlunoGet.cs
@@ -0,0 +1,30 @@
+﻿using AlunoApi.Entities;
+using AlunoApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunoApi.Endpoints
+{
+    public class AlunoGet
+    {
+        public static string Template => "/aluno/{id:int}";
+        public static string[] Metodo => new string[] { HttpMethod.Get.ToString() };
+        public static Delegate Func => Acao;
+
+        public static IResult Acao([FromRoute] int id)
+        {
+            AlunoRepository alunoCarga = new AlunoRepository();
+            alunoCarga.Carga();
+
+            Aluno alunoEcontrado = AlunoRepository.BancoDeDados.FirstOrDefault(a => a.Id.Equals(id));
+
+            if(alunoEcontrado != null)
+            {
+                return Results.Ok(alunoEcontrado);
+            }
+            else
+            {
+                return Results.NotFound();
+            }
+        }
+    }
+}
